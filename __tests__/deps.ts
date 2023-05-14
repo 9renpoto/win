21dbea1316dd35d6fe8f61c0a9052b70272c6cf8
Keisuke Umeno <9renpoto@gmail.com>
2023-05-14 23:14:12 +0900

    refactor: upgrade std

--- __tests__/deps.ts
+++ __tests__/deps.ts
@@ -4,6 +4,5 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-export { assertExists } from "https://deno.land/std@0.150.0/testing/asserts.ts";
-export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 export { render } from "https://esm.sh/@testing-library/preact@3.2.2";
