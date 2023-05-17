e0b86a07e9d1e785ee214021270499bad0201390
Keisuke Umeno <9renpoto@gmail.com>
2023-05-18 07:20:10 +0900

    chore: props

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,5 +1,7 @@
 import { UnknownPageProps } from "$fresh/server.ts";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
-  return <p class="max-w-screen-md w-full px-4 pt-16 mx-auto">404 not found</p>;
+  return (
+    <p class="max-w-screen-md w-full px-4 pt-16 mx-auto">404 not found {url}</p>
+  );
 }
