0fdd8b828230ed69c1a94895f33e88a18b0a155b
Keisuke Umeno <9renpoto@gmail.com>
2023-07-07 11:07:26 +0900

    chore: use ga4

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,81 @@
-import { createReporter } from "$g_a";
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import type { Event } from "$ga4";
+import { GA4Report, isDocument, isServerError } from "$ga4";
 
-const ga = createReporter({ id: "G-5SVZ0B41GD" });
+const GA4_MEASUREMENT_ID = Deno.env.get("GA4_MEASUREMENT_ID");
+
+let showedMissingEnvWarning = false;
+
+function ga4(
+  request: Request,
+  conn: MiddlewareHandlerContext,
+  response: Response,
+  _start: number,
+  error?: unknown,
+) {
+  if (GA4_MEASUREMENT_ID === undefined) {
+    if (!showedMissingEnvWarning) {
+      showedMissingEnvWarning = true;
+      console.warn(
+        "GA4_MEASUREMENT_ID environment variable not set. Google Analytics reporting disabled.",
+      );
+    }
+    return;
+  }
+  Promise.resolve().then(async () => {
+    // We're tracking page views and file downloads. These are the only two
+    // HTTP methods that _might_ be used.
+    if (!/^(GET|POST)$/.test(request.method)) {
+      return;
+    }
+
+    // If the visitor is using a web browser, only create events when we serve
+    // a top level documents or download; skip assets like css, images, fonts.
+    if (!isDocument(request, response) && error == null) {
+      return;
+    }
+
+    let event: Event | null = null;
+    const contentType = response.headers.get("content-type");
+    if (/text\/html/.test(contentType!)) {
+      event = { name: "page_view", params: {} }; // Probably an old browser.
+    }
+
+    if (event == null && error == null) {
+      return;
+    }
+
+    // If an exception was thrown, build a separate event to report it.
+    let exceptionEvent;
+    if (error != null) {
+      exceptionEvent = {
+        name: "exception",
+        params: {
+          description: String(error),
+          fatal: isServerError(response),
+        },
+      };
+    } else {
+      exceptionEvent = undefined;
+    }
+
+    // Create basic report.
+    const measurementId = GA4_MEASUREMENT_ID;
+    const report = new GA4Report({ measurementId, request, response, conn });
+
+    // Override the default (page_view) event.
+    report.event = event;
+
+    // Add the exception event, if any.
+    if (exceptionEvent != null) {
+      report.events.push(exceptionEvent);
+    }
+
+    await report.send();
+  }).catch((err) => {
+    console.error(`Internal error: ${err}`);
+  });
+}
 
 export async function handler(
   req: Request,
@@ -22,7 +96,7 @@ export async function handler(
     err = e;
     throw e;
   } finally {
-    ga(
+    ga4(
       req,
       ctx,
       res!,
