ef8939f8fb4336088a8fce5411d56df2cffaf5cd
Keisuke Umeno <9renpoto@gmail.com>
2023-07-12 23:30:42 +0900

    fix: remove HEAD

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,21 +1,11 @@
 import { AppProps } from "$fresh/server.ts";
 import { Footer } from "@/components/Footer.tsx";
 import { Header } from "@/components/Header.tsx";
-import { description, title } from "@/utils/website.ts";
-import { Head } from "$fresh/runtime.ts";
-import { SEO } from "@/components/SEO.tsx";
+import { title } from "@/utils/website.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <html lang="ja">
-      <Head>
-        <SEO
-          title={title}
-          description={description}
-          keywords={["life"].join(",")}
-          ogImage="https://avatars3.githubusercontent.com/u/520693?s=460&v=4"
-        />
-      </Head>
       <body class="flex flex-col min-h-screen">
         <Header title={title} />
         <Component />
