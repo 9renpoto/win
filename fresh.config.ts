0d977afa5e59c3b52c4c9a7ba5a0aa5bf46679a8
Keisuke Umeno <9renpoto@gmail.com>
2024-02-07 22:57:15 +0900

    refactor: use fresh_testing_library

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import { freshSEOPlugin } from "$fresh_seo/mod.ts";
+import { freshSEOPlugin } from "$fresh_seo";
 import manifest from "./fresh.gen.ts";
 
 export default defineConfig({
