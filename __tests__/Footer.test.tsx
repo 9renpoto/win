b7060296cf4e3b396fd3fe161a251edf66e1e5d3
Keisuke Umeno <9renpoto@gmail.com>
2024-02-11 09:52:13 +0900

    test(__tests__): add test

--- __tests__/Footer.test.tsx
+++ __tests__/Footer.test.tsx
@@ -1,15 +1,15 @@
 import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
 import { assertExists } from "$std/testing/asserts.ts";
 import { cleanup, render, setup } from "$fresh-testing-library/components.ts";
-import { Header } from "@/components/Header.tsx";
+import { Footer } from "@/components/Footer.tsx";
 
-describe("Header", () => {
+describe("Footer", () => {
   beforeAll(setup);
   afterEach(cleanup);
 
   it("should exists.", () => {
     const { container } = render(
-      <Header title="title" />,
+      <Footer />,
     );
 
     assertExists(container);
--- __tests__/SEO.test.tsx
+++ __tests__/SEO.test.tsx
@@ -1,15 +1,20 @@
 import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
 import { assertExists } from "$std/testing/asserts.ts";
 import { cleanup, render, setup } from "$fresh-testing-library/components.ts";
-import { Header } from "@/components/Header.tsx";
+import { SEO } from "@/components/SEO.tsx";
 
-describe("Header", () => {
+describe("Footer", () => {
   beforeAll(setup);
   afterEach(cleanup);
 
   it("should exists.", () => {
     const { container } = render(
-      <Header title="title" />,
+      <SEO
+        title="title"
+        description="description"
+        ogImage="ogImage"
+        keywords="keywords"
+      />,
     );
 
     assertExists(container);
