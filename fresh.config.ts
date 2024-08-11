179e892ce62f1faa517e94230ad0341870d57d3b
Keisuke Umeno <9renpoto@gmail.com>
2024-08-12 08:25:04 +0900

    feat: add sentry

--- fresh.config.ts
+++ fresh.config.ts
@@ -3,6 +3,19 @@ import tailwind from "$fresh/plugins/tailwind.ts";
 import { freshSEOPlugin } from "$fresh_seo";
 import manifest from "./fresh.gen.ts";
 import { getPosts } from "./utils/posts.ts";
+import * as Sentry from "@sentry/deno";
+
+const dsn = Deno.env.get("SENTRY_DSN");
+
+if (dsn) {
+  Sentry.init({
+    dsn,
+    sampleRate: 1.0,
+    tracesSampleRate: 1.0,
+  });
+} else {
+  console.warn("SENTRY_DSN environment not set");
+}
 
 const posts = await getPosts();
 
