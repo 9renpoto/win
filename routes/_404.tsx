fe1211676262f8f805c779f1ac00b22e6a466e76
Keisuke Umeno <9renpoto@gmail.com>
2025-09-13 08:33:03 +0900

    Update _404.tsx

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -2,6 +2,6 @@ import type { UnknownPageProps } from "$fresh/server.ts";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
-    <p class="max-w-screen-lg w-full px-4 pt-16 mx-auto">404 not found {url}</p>
+    <p class="max-w-screen-lg w-full px-4 pt-16 mx-auto">404 Not found {url}</p>
   );
 }
