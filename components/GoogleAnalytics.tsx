438941425f9b92fea113ff8b129557c52b76f5d5
Keisuke Umeno <9renpoto@gmail.com>
2025-09-29 21:31:12 +0900

    feat: track analytics client side

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+import { Head } from "$fresh/runtime.ts";
+
+interface GoogleAnalyticsProps {
+  measurementId?: string;
+}
+
+export function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
+  if (!measurementId) {
+    return null;
+  }
+
+  const inlineScript = `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${measurementId}');`;
+
+  return (
+    <Head>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+      />
+      <script type="text/javascript">
+        {inlineScript}
+      </script>
+    </Head>
+  );
+}
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,15 @@
 import type { PageProps } from "$fresh/server.ts";
 import { Footer } from "@/components/Footer.tsx";
+import { GoogleAnalytics } from "@/components/GoogleAnalytics.tsx";
 import { Header } from "@/components/Header.tsx";
 import { title } from "@/utils/website.ts";
+
+const GA4_MEASUREMENT_ID = Deno.env.get("GA4_MEASUREMENT_ID");
 
 export default function App({ Component }: PageProps) {
   return (
     <html lang="ja">
+      <GoogleAnalytics measurementId={GA4_MEASUREMENT_ID ?? undefined} />
       <body class="flex flex-col min-h-screen">
         <Header title={title} />
         <Component />
